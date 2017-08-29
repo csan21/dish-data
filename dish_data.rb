1633dc684a780db5fe8e6b09ccbfa459ffa6ca22
csan21 <chrisswho@gmail.com>
2017-08-29 01:10:36 -0500

    Final edit, improve comments and readability

--- dish_data.rb
+++ dish_data.rb
@@ -5,44 +5,51 @@ class DishData
   attr_accessor :dishes
 
   def initialize
-    @dishes = []
     @target_total = 15.05
+    @dishes = []
   end
 
   def load_dishes(filename)
     CSV.foreach(filename, headers: true) do |row|
-      @dishes << Dish.new(name: row[0], price: row[1].scan(/[.0-9]/).join.to_f) # regex to remove the $ from the csv price and change it to float
+      @dishes << Dish.new(name: row[0],
+                          price: row[1].scan(/[.0-9]/).join.to_f) # RegEx to remove the '$' and change to float
     end
   end
 
   def list_dishes
     @dishes.each do |dish|
-      # %.2f to ensure 2 decimal places even for 0
+      # %.2f to ensure 2 decimal places to show trailing zero
       puts "#{dish.name} - $#{format('%.2f', dish.price)}"
     end
   end
 
   def check_for_target
-    total = []
-    for i in 0..(@dishes.size) do
-      # iterates through collection and selects the combination of dishes that have a sum of the target total
-      @dishes.combination(i).to_a.select { |dish| total << dish if dish.sum(&:price) == @target_total }
+    total  = []
+    length = 0..@dishes.size
+    # iterates through and selects the combination of dishes that have a sum equal to target total
+    for i in length do
+      @dishes.combination(i).to_a
+             .select { |dish| total << dish if dish.sum(&:price) == @target_total }
     end
-
     current_total = total.flatten.sum(&:price)
-    # conditions for meeting the target total
-    if current_total == @target_total
-      total.each do |item|
+    # checks for meeting the target total
+    if check_sum?(current_total)
+      total.each do |dish|
         puts '-----------------------------'
         puts 'DISHES THAT CAN BE BOUGHT'
         puts '-----------------------------'
-        item.each(&:print_info)
+        dish.each(&:print_info)
         puts '-----------------------------'
         puts "ADDS UP TO $#{current_total}! \n\n"
       end
     else
       puts '-----------------------------------------------'
-      puts 'No combinations of dishes meet the target total'
+      puts "No combinations of dishes meet the target total \n\n"
     end
+  end
+
+  # returns boolean
+  def check_sum?(total)
+    total == @target_total
   end
 end
