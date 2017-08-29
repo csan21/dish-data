614333e57ead8b5ca4faa3d54c53dca3b28cfd85
csan21 <chrisswho@gmail.com>
2017-08-28 22:19:53 -0500

    Clean up

--- runner.rb
+++ runner.rb
@@ -6,14 +6,32 @@ if ARGV.any?
 
   if    ARGV[0] == 'first'
     dish_data.load_dishes('data_one.csv')
+    puts '------------------'
+    puts 'Meal list number 1'
+    puts '------------------'
+    dish_data.list_dishes
     dish_data.check_for_target
+
   elsif ARGV[0] == 'second'
     dish_data.load_dishes('data_two.csv')
+    puts '------------------'
+    puts 'Meal list number 2'
+    puts '------------------'
+    dish_data.list_dishes
     dish_data.check_for_target
+
   elsif ARGV[0] == 'third'
     dish_data.load_dishes('data_three.csv')
+    puts '------------------'
+    puts 'Meal list number 3'
+    puts '------------------'
+    dish_data.list_dishes
     dish_data.check_for_target
+
   else
-    puts 'Please enter first, second, or third as an argument'
+    raise 'Please enter first, second, or third as an argument'
   end
+
+else
+  raise "Type in 'first', 'second', or 'third' after ruby runner.rb"
 end
